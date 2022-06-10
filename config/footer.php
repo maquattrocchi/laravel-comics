b2b70e7a561dfcc2baeeefd7ee0126a06f4f65e6
maquattrocchi <martinaquattrocchi5@gmail.com>
2022-06-10 17:00:43 +0200

    dati footer

---
+++ config/footer.php
@@ -0,0 +1,112 @@
+<?php
+return[
+    'dcComicsList' => [
+        [
+            'testo' => 'Characters',
+            'url' => '#',
+        ],
+        [
+            'testo' => 'Comics',
+            'url' => '#',
+        ],
+        [
+            'testo' => 'Movies',
+            'url' => '#',
+        ],
+        [
+            'testo' => 'TV',
+            'url' => '#',
+        ],
+        [
+            'testo' => 'Games',
+            'url' => '#',
+        ],
+        [
+            'testo' => 'Videos',
+            'url' => '#',
+        ],
+        [
+            'testo' => 'News',
+            'url' => '#',
+        ],
+    ],
+    'shopList' => [
+        [
+            'testo' => 'Shop DC',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'Shop DC Collectibles',
+            'url' => '#'
+        ]
+    ],
+    'dcList '=> [
+        [
+            'testo' => 'Terms Of Use',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'Privacy policy (New)',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'Ad Choices',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'Advertising',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'Jobs',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'Subscriptions',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'Talent Workshop',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'CPSC Certificates',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'Ratings',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'Shop Help',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'Contact Us',
+            'url' => '#'
+        ],
+    ],
+    'sitesList' => [
+        [
+            'testo' => 'DC',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'MAD Magazine',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'DC Kids',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'DC Univers',
+            'url' => '#'
+        ],
+        [
+            'testo' => 'DC Power Visa',
+            'url' => '#'
+        ],
+    ]
+]
+?>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,2 @@
 <footer>
-    <h1>sono footer</h1>
 </footer>
